1c5df36fb8a0383768e299d07f450f290a4c29c7
ArdenGabriele <Everythingvideomaster@gmail.com>
2024-09-25 12:40:53 -0400

    Add project files.

---
+++ Lab 5 More like my guy hahahhahahaha/Lab 5 More like my guy hahahhahahaha.cpp
@@ -0,0 +1,18 @@
+// Lab 5 More like my guy hahahhahahaha.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using  namespace std;
+
+int main()
+{
+    vector<char> cypher({ 'V', 'F', 'X', 'B', 'L', 'I', 'T', 'Z', 'J', 'R', 'P', 'H', 'D', 'K', 'N', 'O', 'W', 'S', 'G', 'U', 'Y', 'Q', 'M', 'A', 'C', 'E' });
+    string message;
+
+    cout << "Input text to cypher:";
+    getline(cin, message);
+    cout << "Encoded Message:";
+}
